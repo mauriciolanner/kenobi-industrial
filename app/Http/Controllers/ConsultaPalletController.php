afc4cf86d6048d779b1b4878d9e4534dad13efe2
Mauricio Freitas <mauricio.freitas@bomix01.dns>
2023-08-23 16:55:28 -0300

    leitor de op

--- app/Http/Controllers/ConsultaPalletController.php
+++ app/Http/Controllers/ConsultaPalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ConsultaPalletController extends Controller
 {
@@ -14,5 +15,30 @@ class ConsultaPalletController extends Controller
             'NotLoginPages/ConsultaOP',
             []
         );
+    }
+
+    public function APIVerificaPalet(Request $request)
+    {
+        $dados = null;
+        $pallet = null;
+
+        if ($request->op != '') {
+            if (strlen($request->op) > 6)
+                $pallet = substr($request->op, 11, 6);
+            else
+                $pallet = $request->op;
+
+            $dados =  DB::connection('protheus')->select(DB::raw("Exec P12Oficial.dbo.[Bomix_Rastreia_Palete] '$pallet'"));
+
+            return response()->json([
+                'pallet' => $dados,
+                'status' => true
+            ]);
+        }
+
+        return response()->json([
+            'pallet' => $dados,
+            'status' => false
+        ]);
     }
 }
