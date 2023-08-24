b5f5b8ffe107fe9414f8a9edc4db27594ad8574c
Mauricio Freitas <mauricio.freitas@bomix01.dns>
2023-08-24 09:12:35 -0300

    correção do leitor

--- resources/js/Pages/NotLoginPages/ConsultaOP.vue
+++ resources/js/Pages/NotLoginPages/ConsultaOP.vue
@@ -5,11 +5,25 @@
                 <button class="btn btn-success w-100 mt-4 btn-camera" @click="camera = true, pallet = null"><i
                         class="bi bi-camera-fill"></i></button>
             </div>
+        </div>
+        <div class="row">
             <div class="col-md-12">
                 <div class="card bg-white card-login shadow-sm p-3 mt-4">
-
+                    <div class="row">
+                        <div class="col-10">
+                            <label class="form-label">Num etiqueta / Num palete</label>
+                            <input type="text" class="form-control" v-model="codigo" v-on:keyup.enter="carregaDados">
+                        </div>
+                        <div class="col-2">
+                            <button class="btn btn-info w-100" @click="carregaDados" style="margin-top: 29px;">
+                                <i class="bi bi-search"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="row">
             <div class="col-md-12">
                 <div v-if="camera" class="card bg-white card-login shadow-sm p-3 mt-4">
                     <qrcode-stream width="100%" :formats="formato" :track="paintOutline" @detect="onDetect"
@@ -21,30 +35,30 @@
                 </div>
                 <div class="card bg-white card-login shadow-sm p-3 mt-4 mb-4">
                     <div class="row" v-if="pallet">
-                        <h3>{{ codigo }}</h3>
+                        <!-- <h3>{{ codigo }}</h3> -->
                         <div v-if="pallet.status" class="col-md-12">
                             <ul class="list-group" v-for="pallet in pallet.pallet">
                                 <li class="list-group-item"><b>Apontamento MES: </b>{{ pallet.APONTAMENTO_MES }}</li>
                                 <li class="list-group-item"><b>OP: </b>{{ pallet.OP }}</li>
+                                <li class="list-group-item"><b>Integrado Mecalux: </b>{{ pallet.RESULTADO }}</li>
+                                <li class="list-group-item"><b>Status: </b>{{ pallet.STATUS }}</li>
                                 <li class="list-group-item"><b>Cod ST: </b>{{ pallet.COD_ST }}</li>
                                 <li class="list-group-item"><b>Data/Hora: </b>{{ pallet.DATA_HORA }}</li>
                                 <li class="list-group-item"><b>Produto: </b>{{ pallet.PRODUTO }}</li>
                                 <li class="list-group-item"><b>Descrição: </b>{{ pallet.DESCRICAO }}</li>
                                 <li class="list-group-item"><b>Qtd: </b>{{ pallet.QUANTIDADE }}</li>
-                                <li class="list-group-item"><b>Erro de integração: </b>{{ pallet.ERRO_INTEGRACAO }}</li>
+                                <li class="list-group-item"><b>Erro de integração MES: </b>{{ pallet.ERRO_INTEGRACAO }}</li>
                                 <li class="list-group-item"><b>Estorno MES: </b>{{ pallet.ESTORNO_MES }}</li>
                                 <li class="list-group-item"><b>Receita: </b>{{ pallet.RECEITA }}</li>
                                 <li class="list-group-item"><b>Recurso: </b>{{ pallet.RECURSO }}</li>
-                                <li class="list-group-item"><b>Req Origem: </b>{{ pallet.REQ_ORIGEM }}</li>
-                                <li class="list-group-item"><b>Resultado: </b>{{ pallet.RESULTADO }}</li>
-                                <li class="list-group-item"><b>Rotina: </b>{{ pallet.ROTINA }}</li>
-                                <li class="list-group-item"><b>Status: </b>{{ pallet.STATUS }}</li>
-                                <li class="list-group-item"><b>ST Del: </b>{{ pallet.ST_DELETADA }}</li>
-                                <li class="list-group-item"><b>ST Palet: </b>{{ pallet.ST_PALETE }}</li>
-                                <li class="list-group-item"><b>ZF9 data: </b>{{ pallet.ZF9_DATA }}</li>
-                                <li class="list-group-item"><b>ZF9_FSCMID: </b>{{ pallet.ZF9_FSCMID }}</li>
-                                <li class="list-group-item"><b>ZF9_HORA: </b>{{ pallet.ZF9_HORA }}</li>
-                                <li class="list-group-item"><b>ZF9_STATUS: </b>{{ pallet.ZF9_STATUS }}</li>
+                                <!-- <li class="list-group-item"><b>Req Origem: </b>{{ pallet.REQ_ORIGEM }}</li> -->
+                                <!-- <li class="list-group-item"><b>Rotina: </b>{{ pallet.ROTINA }}</li> -->
+                                <li class="list-group-item"><b>Etiqueta Palet: </b>{{ pallet.ST_PALETE }}</li>
+                                <li class="list-group-item"><b>Etiqueta Deletada: </b>{{ pallet.ST_DELETADA }}</li>
+                                <li class="list-group-item"><b>Id Mecalux: </b>{{ pallet.ZF9_FSCMID }}</li>
+                                <li class="list-group-item"><b>Data integração Mecalux: </b>{{ pallet.ZF9_DATA }}</li>
+                                <li class="list-group-item"><b>Hora integração mecalux: </b>{{ pallet.ZF9_HORA }}</li>
+                                <li class="list-group-item"><b>Status: </b>{{ pallet.ZF9_STATUS }}</li>
                             </ul>
                         </div>
                         <div v-if="!pallet.status" class="col-md-12">
